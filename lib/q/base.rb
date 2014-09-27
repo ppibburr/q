fbbe474e7f3fc22a73a16a1978c9d77ed49735f1
ppibburr <tulnor33@gmail.com>
2014-09-27 00:37:29 -0400

    ...

--- lib/q/base.rb
+++ lib/q/base.rb
@@ -1,3 +1,17 @@
+# TODO: 
+# Interfaces
+# Fields
+# ! Blocks
+# ! param, return type of array
+# Class instantiate
+# ! primitive array each
+# generic type 
+# case
+# break
+# continue
+# rescue
+# range
+
 module Q
   DECLARE_TYPED = [
     :int,
@@ -28,8 +42,12 @@ module Q
       @buff = @parent.buff
       
       @type = sexp[0]
-      @string = sexp[1]
+      @string = sexp[1] == "self" ? "this" : sexp[1]
     end 
+    
+    def set_buff b
+      @buff = []
+    end
     
     def write;
       if type == :"@tstring_content"
@@ -38,12 +56,17 @@ module Q
       elsif type == :"@ivar"
         buff << "this."
         bool_ivar = true
+      elsif type == :"@float"
+        bool_flt = true
       end
       
       @buff << (bool_ivar ? @string.gsub(/^\@/,'') : @string)
       
       if bool_str
         buff << "\""
+      end
+      if bool_flt
+        buff << "f"
       end
     end
   end
@@ -59,6 +82,10 @@ module Q
     def write
       @buff << @sexp.to_s
     end
+    
+    def set_buff b
+      @buff = b
+    end
   end
 
   class Base
@@ -68,6 +95,8 @@ module Q
       @sexp = sexp
       @parent = parent
     
+      #@flags = parent.flags
+    
       @buff = parent ? parent.buff : []
     
       @children = []
@@ -75,6 +104,10 @@ module Q
       init
       
       perform
+    end
+    
+    def flag flg
+      flags << flg
     end
 
     def x(str)
@@ -100,21 +133,34 @@ module Q
       end
     end
     
+    def set_buff b
+      @buff = b
+      children.each do |c| c.set_buff b end
+    end
+    
     def build_q q
       if !q.is_a?(Array)
         NonArray.new(q, self)
       elsif q[0].to_s =~ /^\@/
         Value.new(q, self)
-      else; p q[0]
+      else
         case q[0]
+        when :class
+          Klass.new(q,self)
         when :assign
           Assign.new q, self
-        when:var_field
+        when :var_field
           VarField.new q, self
+        when :var_ref
+          VarRef.new(q, self)
+        when :aref_field
+          ARefField.new(q,self)
         when :program
           Program.new q, self
         when :method_add_arg
           MethodAddArg.new(q, self)
+        when :method_add_block
+          MethodAddBlock.new(q,self)
         when :args_add_block
           ArgsAddBlock.new q,self
         when :def
@@ -131,11 +177,16 @@ module Q
           Call.new q,self
         when :fcall
           FCall.new(q,self)
-        
+        when :aref
+          ARef.new(q,self)
         else
           Base.new q,self
         end
       end
+    rescue => e
+      puts buff.join
+      puts "VeryDescriptiveError: #{q}\nERROR: #{e.to_s}\n#{e.backtrace[0]}"
+      exit
     end
   end
   
@@ -144,6 +195,19 @@ module Q
       sexp[1].each do |s|
         children << build_q(s)
       end
+    end
+  end
+  
+  class ARefField < Base
+    def perform      
+      super
+    end
+    
+    def write
+       children[0].write
+       buff << "["
+       children[1].write
+       buff << "]"
     end
   end
   
@@ -178,7 +242,7 @@ module Q
         if !c.is_a?(Body)
           @buff << ";"
         end
-        
+
         @buff << "\n"
         
         b.call if b
@@ -189,9 +253,15 @@ module Q
   class FCall < Base
     def write
       super
+      
+      return unless buff.last
+      
       if buff.last == "puts"
         buff.last.replace "stdout.printf"
-        return :puts      
+        return :puts  
+      elsif DECLARE_TYPED.index(n=buff.last.to_sym)
+        buff.last.replace("(#{n}) ")
+        return n
       end
     end
   end
@@ -199,10 +269,19 @@ module Q
   class Program < Body
   end  
   
+  class ARef < Base
+    def write
+      children[0].write
+      buff << "["
+      children[1].write
+      buff << "]"
+    end
+  end
+  
   class MethodAddArg < Base    
     def write
       n = @children[0].write
-      @buff << "("
+      @buff << "(" unless DECLARE_TYPED.index(n)
       if n == :puts and c=children[1].children[0].children[0]
         def c.write
           super
@@ -211,7 +290,7 @@ module Q
       end
       
       @children[1].write
-      @buff << ")"
+      @buff << ")" unless DECLARE_TYPED.index(n)
     end
   end
  
@@ -221,49 +300,106 @@ module Q
     end
   end
   
+  class VarRef < Base
+  
+  end
+  
   class Assign < Base
     def perform
       super
-      if children[0].children[0].type == :"@ident"
+      if children[0].is_a?(VarField) and children[0].children[0].type == :"@ident"
         parent.lvars[children[0].children[0].string] ||= [children[1], false]
+      elsif children[0].is_a?(ARefField)
+        if children[0].children[0].is_a?(VarRef)
+          parent.lvars[children[0].children[0].children[0].string] ||= [children[1], false]
+        end 
       end
     end
   
-    def write
-      bool = parent.lvars[children[0].children[0].string] ? !parent.lvars[children[0].children[0].string][1] : false
-      parent.lvars[children[0].children[0].string][1] = true if parent.lvars[children[0].children[0].string]
-      @buff << "var " if bool
+    def assign_array?()
+      children[1].sexp[0] == :aref and children[1].sexp[1][0] == :vcall
+    end
+  
+    def declare_array()
+      buff << "var "
       children[0].write
-      @buff << " = "
+      buff << " = new "
+      children[1].children[0].write
+      buff << "["
+      children[1].children[1].write
+      buff << "]" 
+    end
+  
+    def assign_lvar?()
+      children[0].sexp[0] == :var_field
+    end
+    
+    def lvar_is_new?
+      if children[0].is_a?(VarField) and children[0].children[0].type == :"@ident"
+        bool = !parent.lvars[children[0].children[0].string][1]
+        return bool
+      end
+    end
+  
+    def declare_lvar()
+      bool = lvar_is_new?
+      parent.lvars[children[0].children[0].string][1] = true if parent.lvars[children[0].children[0].string]    
+    
+      if assign_array?()
+        declare_array
+        
+        return
+      end
+      
+      buff << "var " if bool
+      children[0].write
+      buff << " = "
       write_right(bool)
+    end
+  
+    def write
+      if assign_lvar?()
+        declare_lvar()
+      
+        return
+      end
+      
+      children[0].write
+      buff << " = "
+      write_right(false)
     end
     
     def write_right bool
-      if children[1].is_a?(MethodAddArg)
-      
-        if children[1].sexp[1][0] == :fcall;
-          q = children[1].children[1]
-          n =  children[1].children[0].children[0].sexp[1]
-          if Q::DECLARE_TYPED.index(n.to_sym)
-            if bool
-              buff << "(#{n}) "
-              q.write
-            else
-              children[1].write
-            end
-          else
-            children[1].write
-          end
-        end
-      else
-        children[1].write
-      end
+      children[1].write
     end
   end
   
   class Klass < Body
+    attr_reader :name, :sc, :interfaces
     def init
+      n = sexp.delete_at(3)
+      
+      o = sexp.map do |q| 
+        e = build_q(q)
+        e.instance_variable_set("@buff", [])
+        e
+      end
+      
+      
+      @sexp = n
+      @name = o[1]
+      @sc = o[2]
+      @interfaces = []
+    end
     
+    def write
+      @ident -= 4
+      buff << "\n"
+      name.instance_variable_set("@buff", [])
+      s = sc ? (sc.children[0] ? " : "+sc.children[0].string : nil) : nil
+      x"class #{name.children[0].sexp[1]}#{s} {"
+      super
+      x "}"
     end
   end
   
@@ -271,24 +407,50 @@ module Q
     include HasMany
   end
   
+  module SignatureType
+    def init
+      @buff = []
+    end
+    def write
+      bool = @sexp[0] == :aref
+      super
+      buff << "[]" if bool
+    end  
+  end
+  
+  class ReturnType < Base
+    include SignatureType
+  end
+  
   class Def < Body
-    attr_reader :name, :params
+    attr_reader :name, :params, :return_type
     def init
       n = @sexp.delete_at(3)
       d = @sexp
       @name = d[1][1]
       @params = build_q(d[2])
       @sexp = n
+      
+      if explicit_return_type?
+        q = @sexp[1].delete_at(0)
+        @return_type = ReturnType.new(q,self,0)
+      end
+    end
+    
+    def explicit_return_type?()
+      @sexp[1][0][0] == :symbol_literal or (@sexp[1][0][0] == :aref and @sexp[1][0][1][0] == :symbol_literal)
     end
     
     def write
-      @ident -= 3
-      x("\npublic type #{name}(")
+      return_type.write
+      @ident -= 4
+      buff << "\n"
+      x("public #{return_type.buff.join()} #{name}(")
       params.write
       buff << ") {\n"
       super
       @ident -= 1
-      x("}\n")
+      x(" }\n")
     end
   end
   
@@ -296,7 +458,12 @@ module Q
     attr_reader :name, :type
     def perform
       super
-      @type = children[1]
+      if parent.parent.is_a? Block and !children[1] and parent.parent.type_param
+        @type = parent.parant.type_param
+      else
+        @type = children[1]
+      end
+      
       @name = children[0]
     end
     
@@ -324,11 +491,80 @@ module Q
     end
   end
   
+  class MethodAddBlock < Base
+    def init
+      o = buff
+      blk =  sexp.pop
+      @buff = []
+      q = Base.new sexp, self
+      q.write
+      if q.buff.last == "each"
+        b = Block.new(blk,nil)
+        @foreach = ForEach.new(q.buff.join.split(/\.each$/)[0], b)
+      else
+        sexp << blk
+      end
+      
+      @buff = @parent.buff 
+    end
+    
+    def perform
+      if @foreach
+        
+      else
+        super
+      end
+    end
+    
+    def write
+      if @foreach
+        @foreach.write
+        buff << @foreach.buff.join
+      else
+        super
+      end
+    end
+  end
+  
+  class ForEach
+    attr_reader :buff,:target, :block, :ident
+    def initialize target, blk
+      @target = target
+      @block = blk
+      @ident = 0
+      if blk.children[0] and blk.children[0].children[0]
+
+        @params = blk.params
+      end
+
+      @buff = []
+    end
+    
+    def write
+      buff << "foreach ( "
+      Class.new(Params).new(@params.sexp,self).write
+      buff << " in #{target}) {\n"
+      Class.new(Body).new(@block.sexp, self).write
+      buff << "}\n"
+    end
+  end
+  
+  class Block < Body
+    attr_reader :params
+    def init
+      @params = Params.new(sexp[1][1],self)
+      @sexp = [:bodystmt].push sexp[2]
+    end
+  end
+  
   class Params < Base
+    
     def perform
       sexp[5].each do |q|
         children << Param.new([:param].push(*q), self)
-      end
+      end if sexp[5]
+      
+
     end
     
     def write
@@ -382,20 +618,16 @@ end
 if __FILE__ == $0
 require 'pp'
 code = "
-def n(x:int)
-  this.p(1,2)
-  this.p(1,2)
-  a = 1
-  a = 3
-  a
-  i = char(5)
-  i = foo(4)
-  n = \"fred\"
-  z = 0.0.d
-  puts(\"%f\",z)
-  return(@foo)
-  55.d / 44.0.f
-  @foo = 5
+class Z < Object
+  def n(x:int, y:int[], z:string):int[]
+    foo = int[2]
+    foo[0] = 1
+    foo += 2    
+
+    foo.each do |z:int|
+      print(\"%d\",z+1)
+    end
+  end
 end
 "
 puts Q.translate(code)
